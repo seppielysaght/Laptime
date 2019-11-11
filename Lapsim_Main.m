70a74e2a5fc883231dc3214c384c6a57cfebe678
seppielysaght <lysaght6@gmail.com>
2019-11-11 21:03:22 +0000

    Update to main run file and some extra bits of testing code.

--- Lapsim_Main.m
+++ Lapsim_Main.m
@@ -11,5 +11,21 @@
 %   04/11/2019: Initial creation
 % *************************************************************************
 clear
+
+%Load Track Values 
 load('C:\Laptime\Tracks\CroixTernoisTrack.mat')
-Track = curvature(Track); 
+
+%calculate curvature
+Track = curvature(Track);
+
+%Create Result STRUCT
+Output.steps = zeros(Track.lenght,1);
+
+%Load Car parameters
+load('C:\Laptime\Cars\RoughTestValuesFSCar.mat')
+
+%Calculate Vertical Corner Loads
+Output = cornerWeights(Car,Track,Output);
+
+%Calculate max cornering speed
+Output = maxCorneringSpeed(Track,Car,Output);
